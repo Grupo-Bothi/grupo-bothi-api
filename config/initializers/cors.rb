8bab6a949485f323e6e912195ce7afc6a0042645
Jacinto Bothi <jacinto.bothi@origon.io>
2026-04-22 09:30:23 -0600

    fix(cors): add array for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,12 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("FRONTEND_URL", "FRONTEND_URL_UI", "http://localhost:4200")
+    allowed_origins = [
+      ENV["FRONTEND_URL"],
+      ENV["FRONTEND_URL_UI"],
+      ("http://localhost:4200" if Rails.env.development? || Rails.env.test?)
+    ].compact.reject(&:empty?)
+    origins(*allowed_origins)
 
     resource "*",
       headers:  :any,
