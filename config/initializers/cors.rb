0dc91092254c1dc1f8da4be91c9dbdd3c0f2c522
Jacinto Bothi <jacinto.bothi@origon.io>
2026-04-22 09:21:18 -0600

    fix(ui): add environment variable

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("FRONTEND_URL", "http://localhost:4200")
+    origins ENV.fetch("FRONTEND_URL", "FRONTEND_URL_UI", "http://localhost:4200")
 
     resource "*",
       headers:  :any,
